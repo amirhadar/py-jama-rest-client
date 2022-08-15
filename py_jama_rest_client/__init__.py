9622aaee90087533956ff20f1e486c9f7e92978c
yelmalem <yelmalem@aligntech.com>
2022-08-15 15:33:27 +0300

    another try

---
+++ py_jama_rest_client/__init__.py
@@ -0,0 +1,9 @@
+from .client import JamaClient, jama_client
+from .constants import JamaLabelExpressionConstant
+
+
+__all__ = (
+    'JamaClient',
+    'jama_client',
+    'JamaLabelExpressionConstant',
+)
